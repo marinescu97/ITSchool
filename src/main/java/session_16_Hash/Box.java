fb876c54085f9c153547505fd467b82d94114373
Marinescu Monica <marinescumonica97@yahoo.com>
2025-04-15 20:32:11 +0300

    Added Session 16

---
+++ src/main/java/session_16_Hash/Box.java
@@ -0,0 +1,25 @@
+package session_16_Hash;
+
+public class Box<T>{
+    private T obj;
+
+    public T getObj() {
+        return obj;
+    }
+
+    public void setObj(T obj) {
+        this.obj = obj;
+    }
+}
+
+class BoxTest{
+    public static void main(String[] args) {
+        Box<String> stringBox = new Box<>();
+        stringBox.setObj("Hello");
+        System.out.println(stringBox.getObj());
+
+        Box<Integer> integerBox = new Box<>();
+        integerBox.setObj(23);
+        System.out.println(integerBox.getObj());
+    }
+}
